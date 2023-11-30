38bbdea5bb57f1b1c05726f74b4fbebb5097bf1f
bendibdounia <bendib.dounia@gmail.com>
2023-11-29 16:03:25 -0800

    flash

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
 #!/usr/bin/pup
 # Install flask from pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+package { 'python3-pip':
+  ensure   => installed,
+}
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => '/usr/bin',
+  require => Package['python3-pip'],
 }
