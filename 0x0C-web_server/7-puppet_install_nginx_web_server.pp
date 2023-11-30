32aacfb688c5486184de9426cf0684fddbc28ed3
bendibdounia <bendib.dounia@gmail.com>
2023-11-29 17:20:09 -0800

    don

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,7 +1,13 @@
 #installing ngix
 
+exec { 'apt-get-update':
+  command => '/usr/bin/apt-get update',
+}
+
 package { 'nginx':
-  ensure => 'installed'
+  ensure  => installed,
+  require => Exec['apt-get-update'],
+
 }
 
 file { '/var/www/html/index.html':
@@ -9,10 +15,10 @@ file { '/var/www/html/index.html':
 }
 
 file_line { 'redirection-301':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 service { 'nginx':
