dc58e2699f4e74f3d5f8fc556ec86f57a69ad67a
bendib.dounia <bendib.dounia@gmail.com>
2024-01-16 13:25:42 -0800

    subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,8 +8,13 @@ import request
 def number_of_subscribers(subreddit):
     """fct that return subreddit or 0 if invalid""" 
     url = f"https://www.reddit.com/r/{}/about.json"
+
+    # Get user agent
+    # Reference: https://stackoverflow.com/questions/10606133/
+    # sending-user-agent-using-requests-library-in-python
     headers = requests.utils.default_headers()
     headers.update({'User-Agent': 'My User Agent 1.0'})
+    
     resp = requests.get(url, headers=headers).json()
     subscribers = resp.get('data', {}).get('subscribers')
     if not subscribers:
