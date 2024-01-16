9b9ba190cdd7859ffdfc4d3cff8751acd33bd455
bendib.dounia <bendib.dounia@gmail.com>
2024-01-16 13:21:53 -0800

    subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and returns the number of subscribers
+"""
+import request
+
+
+def number_of_subscribers(subreddit):
+    """fct that return subreddit or 0 if invalid""" 
+    url = f"https://www.reddit.com/r/{}/about.json"
+    headers = requests.utils.default_headers()
+    headers.update({'User-Agent': 'My User Agent 1.0'})
+    resp = requests.get(url, headers=headers).json()
+    subscribers = resp.get('data', {}).get('subscribers')
+    if not subscribers:
+        return 0
+    return subscribers
