bd8034b2167ee212681a1f6669f923adbe4023bf
bendib.dounia <bendib.dounia@gmail.com>
2024-01-25 15:56:40 -0800

    done

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # INcrease the ULIMIT value
 exec {'24: Too many open files':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  command => 'sed -i "s/15/4098/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 } ->
 exec {'Restart nginx':
---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,10 @@
+#Change the OS configuration
+exec {'hard-type':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec {'soft-type':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
